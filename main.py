f5c9f6834c46b753b4919e66584c6209514696f9
jspoleti66 <jspoletti@yahoo.com.ar>
2025-05-17 16:31:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import Flask, request, jsonify
 
 from flask import render_template
 
+app = Flask(__name__)
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
