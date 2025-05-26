d8700174b1c45d23cb4d172d9aaaf02a1961baa5
jspoleti66 <jspoletti@yahoo.com.ar>
2025-05-26 09:57:21 -0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+let peerConnection;
 document.getElementById("start-button").onclick = async () => {
   try {
     const response = await fetch("/api/init", { method: "POST" });
@@ -18,6 +19,63 @@ document.getElementById("start-button").onclick = async () => {
       console.log("WebSocket conectado.");
     };
 
+    socket.onmessage = async (event) => {
+      const msg = JSON.parse(event.data);
+
+      if (msg.type === "offer") {
+        console.log("Recibido SDP offer:", msg);
+
+        const config = {
+          iceServers: [
+            {
+              urls: [
+                "stun:stun.cloudflare.com:3478",
+                "turn:turn.cloudflare.com:3478?transport=udp",
+                "turn:turn.cloudflare.com:3478?transport=tcp",
+                "turns:turn.cloudflare.com:5349?transport=tcp",
+                "turn:turn.cloudflare.com:80?transport=tcp",
+                "turns:turn.cloudflare.com:443?transport=tcp"
+              ],
+              username: "g0442535f9d1e8f0ee98823899be1bcbe84cc966baa34a49bfb77706c71e6c8c",
+              credential: "20482627d9bc784e1a56535f5add2d9179e4456a5b5f2fe324b174b4bc758b3c"
+            }
+          ]
+        };
+
+        peerConnection = new RTCPeerConnection(config);
+
+        peerConnection.ontrack = (event) => {
+          const remoteVideo = document.getElementById("remoteVideo");
+          if (remoteVideo.srcObject !== event.streams[0]) {
+            remoteVideo.srcObject = event.streams[0];
+            console.log("Video conectado");
+          }
+        };
+
+        peerConnection.onicecandidate = (event) => {
+          if (event.candidate) {
+            socket.send(JSON.stringify({ type: "candidate", candidate: event.candidate }));
+          }
+        };
+
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        socket.send(JSON.stringify({ type: "answer", sdp: answer.sdp }));
+        console.log("Respuesta enviada");
+      }
+
+      if (msg.type === "candidate") {
+        try {
+          await peerConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
+          console.log("ICE candidate agregado");
+        } catch (e) {
+          console.error("Error al agregar ICE candidate:", e);
+        }
+      }
+    };
+
+    
     socket.onerror = (err) => {
       console.error("WebSocket error:", err);
     };
