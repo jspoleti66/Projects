0af19746c1e7552dd938c5be4fe9a86ed6764384
jspoleti66 <jspoletti@yahoo.com.ar>
2025-06-11 11:20:08 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-pip install torch==2.0.0 torchaudio==2.0.1
+#!/bin/bash
+
+# Instalación de torch compatible con torchaudio
+pip install torch==2.0.0+cpu torchvision==0.15.1+cpu torchaudio==2.0.1+cpu --index-url https://download.pytorch.org/whl/cpu
+
+# Instalar resto de dependencias
 pip install -r requirements-core.txt
